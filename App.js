b728e10a5a1978d1488a9eeffaf0e5dce083c329
sonu001729 <114768362+sonu001729@users.noreply.github.com>
2023-06-04 12:58:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SetCadetLoc from './MyComponents/SetCadetLoc';
+
+function App() {
+  return (
+    <div className="App">
+      <SetCadetLoc />
+    </div>
+  );
+}
+
+export default App;
